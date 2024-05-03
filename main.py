9b447f68c126b3f7b73bb673bc76ced2bf2fa841
Robo-Hermano <138218435+Robo-Hermano@users.noreply.github.com>
2024-05-03 17:55:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #NOTE: bad idea to run this all in one segment, would advise using a
-#jupyter notebook instead - which is what I actually used for this project
+#jupyter notebook instead and running each block separately
 
-
+#block 1
 #import needed modules
 import tensorflow as tf
 import keras
@@ -12,12 +12,33 @@ import numpy as np
 import pandas as pd
 import math
 from sklearn.preprocessing import LabelEncoder #this was a mistake, this is meant for the label, not features
+
+#block 2
+#get our data in
 #get our data in
 trainset = pd.read_csv("titanic_train.csv")
 testset = pd.read_csv("titanic_test.csv")
 trainset = trainset.reindex(np.random.permutation(trainset.index))
 trainset = trainset.fillna(method = "pad")
 testset = testset.fillna(method = "pad")
+print(trainset.isnull().sum())
+testset.isnull().sum()
+value_count = sum(trainset[trainset.columns[i]].count() for i in range(8))
+nan_count = sum(trainset[trainset.columns[i]].isnull().sum() for i in range(8))
+percent_missing = nan_count/(value_count+nan_count)*100
+value_count2 = sum(testset[testset.columns[i]].count() for i in range(8))
+nan_count2 = sum(testset[testset.columns[i]].isnull().sum() for i in range(8))
+percent_missing2 = nan_count2/(value_count2+nan_count2)*100
+print(f"{percent_missing} for trainset and {percent_missing2} for testset")
+
+#block 3
+#start to see some correlations and datatypes
+print(trainset.dtypes)
+print(trainset.columns)
+print(trainset.shape)
+print(trainset["Sex"])
+
+#block 4
 #build the feed-forward neural network model
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(7,)),
@@ -31,6 +52,8 @@ model.compile(
     loss = "binary_crossentropy",
     metrics = ["accuracy"]
 )
+
+#block 5
 def transform(value):
     hash = {0:"Z",1:"O",2:"T",3:"P"}
     value = hash[value]
@@ -43,6 +66,13 @@ trainset["Pclass"] = encode.fit_transform(trainset["Pclass"])
 testset["Pclass"] = encode.fit_transform(testset["Pclass"])
 trainset["Embarked"] = encode.fit_transform(trainset["Embarked"])
 testset["Embarked"] = encode.fit_transform(testset["Embarked"])
+
+#block 6
+qualitative = ["Age","SibSp","Parch","Fare"]
+for i in qualitative:
+    trainset.hist(i)
+
+#block 7
 def logging(value):
     try:
         value = math.log(value,10)
@@ -56,11 +86,15 @@ trainset["Fare"],trainset["SibSp"],trainset["Parch"] = trainset["Fare"].apply(lo
 trainset["Age"] = trainset["Age"].apply(scaling)
 testset["Fare"],testset["SibSp"],testset["Parch"] = testset["Fare"].apply(logging),testset["SibSp"].apply(logging),testset["Parch"].apply(logging)
 testset["Age"] = testset["Age"].apply(scaling)
+
+#block 8
 ytrain = trainset["Survived"]
 xtrain = trainset.drop(columns=["Survived","PassengerId","Name","Ticket","Cabin"])
 xtest = testset.drop(columns=["PassengerId","Name","Ticket","Cabin"])
 model.fit(xtrain,ytrain,validation_split=0.2,epochs=200)
 predictions = model.predict(xtest)
+
+#block 9
 predictions_binary = np.round(predictions).astype(int)
 final_predictions = []
 for i in predictions_binary:
